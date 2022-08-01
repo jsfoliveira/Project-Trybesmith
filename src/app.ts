10d7f5877bbc4f88a62f6eb5c32623475507eee8
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-01 14:50:50 -0300

    Consertando erro

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import productRouter from './routes/product.route';
 import userRouter from './routes/user.route';
 import orderRouter from './routes/order.route';
 
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/products', userRouter);
+app.use('/products', productRouter);
 
 app.use('/users', userRouter);
 
